18fefd443a5c7476acc5958573db88da2b19e846
Brayden Wu <116959879+Brayden-wu@users.noreply.github.com>
2023-04-20 10:54:15 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,19 +1,13 @@
-// public class Question1
-// {
-//   public static void main(String[] args)
-//   {
-//     /**
-//      * Print "Hello World!"
-//      * 
-//      * Hint: System.out.println(...)
-//      */
-//     System.out.println("Hello world!");
-//   }
-// }
-
-
-public class Question1 {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+public class Question1
+{
+  public static void main(String[] args)
+  {
+    /**
+     * Print "Hello World!"
+     * 
+     * Hint: System.out.println(...)
+     */
+    System.out.println("Hello World!");
   }
 }
+
